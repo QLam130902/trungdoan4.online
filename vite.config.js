a1b9b8e0604fafd892178be2ee36e3c6ba53c92c
QLam130902 <lamdo.130902@gmail.com>
2026-05-03 09:27:58 +0700

    feat: deploy với tên miền trungdoan4.io.vn

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-  // Tự nhận biết: build (lên Github Pages) thì dùng /trungdoan4.online/, chạy dev (local) thì dùng /
-  base: command === 'build' ? '/trungdoan4.online/' : '/'
-}))
+  base: '/'
+})
